bc8dc5450233d5420c217022f79801df5cf7c365
Don-Vito <khvitaly@gmail.com>
2013-06-09 20:57:41 +0300

    Try to Apply Same DataTemplate Selector to All Cells

---
+++ LazyLog/LogRecordCellTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using LazyLog.LogProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace LazyLog
+{
+    public class LogRecordCellTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SeverityImageTemplate
+        {
+            get;
+            set;
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Severity)
+            {
+                return SeverityImageTemplate;
+            }       
+     
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
